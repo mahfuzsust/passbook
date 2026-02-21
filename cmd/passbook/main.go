0ed761c31a9c5528c7bb890a0443568f60895aa6
Mahfuzur Rahman <mahfuzur.rahman@soundcloud.com>
2026-02-21 17:35:40 +0100

    add version

--- cmd/passbook/main.go
+++ cmd/passbook/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"time"
 
 	"passbook/internal/config"
 	"passbook/internal/ui"
 )
 
+var version = "dev"
+
 func main() {
+	showVersion := flag.Bool("version", false, "print version and exit")
+	flag.Parse()
+	if *showVersion {
+		fmt.Println(version)
+		return
+	}
+
 	cfg := config.LoadOrInit()
 
 	h, err := ui.NewApp(cfg)
